6703bc54f307aedaf7b1c6752879359871b40789
Corentin Thomasset <corentin.thomasset74@gmail.com>
2022-05-05 17:59:47 +0200

    refactor: noun phrases

--- packages/generator/src/models/nounPhrase.ts
+++ packages/generator/src/models/nounPhrase.ts
@@ -1,13 +1,13 @@
 import { adjective } from './../generators/adjective';
 import { getRandomItemInArray } from '@autheur/operators';
 import type { Generator } from '../generator';
-import { adverb, determinant, noun, word } from '../generators';
+import { adverb, determinant, noun, oneOf, word } from '../generators';
 
 export function nounPhrase(): Generator[] {
   return getRandomItemInArray([
-    [determinant(), noun()],
     [determinant(), noun(), adjective()],
+    [determinant(), adjective(), noun()],
+    [determinant(), adjective(), noun(), adverb(), adjective()],
     [determinant(), noun(), adverb(), adjective()],
-    [determinant(), noun(), adjective(), word('et'), adjective()],
   ]);
 }
